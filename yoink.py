e9c55a4d192fd99e2557da548b4c8ce5b8d6ddb4
DumbNerd <btron@me.com>
2022-03-10 20:05:31 +1000

    Create yoink.py

---
+++ yoink.py
@@ -0,0 +1,21 @@
+from requests import get
+from bs4 import BeautifulSoup as BS
+import re
+
+
+print('********Use https:// or http://********')
+targeturl = input('Yoink URL =')
+url = targeturl
+
+response = get(url)
+soup = BS(response.content, "html.parser")
+for child in soup.body.children:
+   if child.name == 'script':
+       child.decompose()
+words = re.sub(r'\n\s*\n', r'\n\n', soup.get_text().strip(), flags=re.M)
+
+#print(soup.body.get_text())
+#print(soup.a.prettify())
+#print(soup.find().get_text().stuff)
+print(words)
+print('*** END YOINK ***')
